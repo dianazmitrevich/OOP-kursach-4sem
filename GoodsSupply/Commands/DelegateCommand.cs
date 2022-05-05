e7ff3a473e14d74d8ee89059ae6d23751a8182ab
dianazmitrevich <zmitrevichdiana@gmail.com>
2022-05-05 11:56:58 +0300

    added an assistant to bind password in the passwordbox

---
+++ GoodsSupply/Commands/DelegateCommand.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoodsSupply.Commands
+{
+    class DelegateCommand : Command
+    {
+        private readonly Func<object, bool> canExecute;
+        private readonly Action<object> execute;
+
+        public DelegateCommand(Action<object> executeParameter, Func<object, bool> canExecuteParameter = null)
+        {
+            execute = executeParameter ?? throw new ArgumentNullException(nameof(Execute));
+            canExecute = canExecuteParameter;
+        }
+
+        public override bool CanExecute(object parameter) => canExecute?.Invoke(parameter) ?? true;
+
+        public override void Execute(object parameter) => execute(parameter);
+    }
+}
--- GoodsSupply/ViewModels/AuthorizationViewModel.cs
+++ GoodsSupply/ViewModels/AuthorizationViewModel.cs
@@ -1,12 +1,54 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using GoodsSupply.Models;
+using System.Windows.Input;
+using GoodsSupply.Commands;
+using System.Windows;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Windows.Controls;
+using GoodsSupply.Views;
+using GoodsSupply.Views.UserControls;
 
 namespace GoodsSupply.ViewModels
 {
-    class AuthorizationViewModel
+    class AuthorizationViewModel : BaseViewModel
     {
+        private readonly GoodsSupplyContext context = new GoodsSupplyContext();
+
+        private string login;
+        private string password;
+        private string name;
+        private string email;
+
+        public string Login
+        {
+            get => login;
+            set => Set(ref login, value);
+        }
+        public string Password
+        {
+            get => password;
+            set => Set(ref password, value);
+        }
+        public string Name
+        {
+            get => name;
+            set => Set(ref name, value);
+        }
+        public string Email
+        {
+            get => email;
+            set => Set(ref email, value);
+        }
+
+        public ICommand LoginCommand { get; }
+        private bool CanLoginCommandExecute(object p) => Login?.Length > 0 && Password?.Length > 0;
+        private void OnLoginCommandExecuted(object p)
+        {
+            MessageBox.Show(Password);
+        }
+
+        public AuthorizationViewModel()
+        {
+            LoginCommand = new DelegateCommand(OnLoginCommandExecuted, CanLoginCommandExecute);
+        }
     }
 }
